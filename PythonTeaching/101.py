26d0a9d9c45933701443033d236182cd3cf4bf9f
Sierra Wong <wangzirui@mails.cust.edu.cn>
2025-04-12 23:23:18 +0800

    Initialize

--- PythonTeaching/101.py
+++
@@ -1,9 +0,0 @@
-today = 'ap'
-tomorrow = 'lunes'
-hour = 0
-
-while today == 'ap' and hour < 24:
-    print("Love", today, "at", hour, "o'clock.")
-    hour = hour + 1
-
-print("Today is a good day!")
